aec9c1b8cbef4bc4cbaac79f422dd7c07027e08f
Matsiee <v.materzu@gmail.com>
2025-03-24 23:04:45 +0100

    initial commit

--- ConsoleGame2/Program.cs
+++ ConsoleGame2/Program.cs
@@ -6,24 +6,33 @@ internal class Program
     {
         List<Level> levels = new();
 
-        var level1 = new[,]
+        using (var sr = new StreamReader("C:\\Users\\vmate\\RiderProjects\\ConsoleGame2\\ConsoleGame2\\Levels.txt"))
         {
-            { '#', '#', '#', '#', '#', '#' },
-            { '#', '0', '0', '0', '0', '#' },
-            { '#', '0', '0', '0', '0', '#' },
-            { '#', '0', 'B', '0', '0', '#' },
-            { '#', '0', '0', '0', '0', '#' },
-            { '#', '#', '#', '#', '#', '#' }
-        };
-        levels.Add(new Level(level1));
-        var map = level1;
+            Console.WriteLine("Reading levels...");
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                var levelLines = new List<string>();
+                while (!string.IsNullOrEmpty(line))
+                {
+                    levelLines.Add(line);
+                    line = sr.ReadLine();
+                }
 
-        var playerRow = 1;
-        var playerCol = 1;
-        var boxes = new List<(int, int)>();
+                var map = new char[levelLines.Count, levelLines[0].Length];
+                for (var i = 0; i < levelLines.Count; i++)
+                for (var j = 0; j < levelLines[i].Length; j++)
+                    map[i, j] = levelLines[i][j];
+
+                levels.Add(new Level(map));
+            }
+        }
+
+        var mapIndex = 0;
+        var currentLevel = levels[mapIndex];
+        var correctBoxCount = 0;
+
         bool running;
-
-        var debug = "nothing to see here";
         do
         {
             running = RunGame();
@@ -31,29 +40,35 @@ internal class Program
 
         bool RunGame()
         {
+            var map = currentLevel.Map;
+            var playerRow = currentLevel.PlayerPosition.Row;
+            var playerCol = currentLevel.PlayerPosition.Col;
             Console.Clear();
-            Console.WriteLine(debug);
+            Console.WriteLine($" Level {mapIndex + 1}\n");
             for (var row = 0; row < map.GetLength(0); row++)
             {
                 for (var col = 0; col < map.GetLength(1); col++)
-                    if (playerRow == row && playerCol == col)
-                        Console.Write(".@");
-                    else
-                        switch (map[row, col])
-                        {
-                            case '#':
-                                Console.Write(" #");
-                                break;
-                            case '0':
-                                Console.Write("..");
-                                break;
-                            case 'B':
-                                Console.Write("[]");
-                                break;
-                            case ' ':
-                                Console.Write("  ");
-                                break;
-                        }
+                    switch (currentLevel.GetTile(row, col))
+                    {
+                        case 'P':
+                            Console.Write(".@.");
+                            break;
+                        case 'B':
+                            Console.Write("[-]");
+                            break;
+                        case '#':
+                            Console.Write(" # ");
+                            break;
+                        case '0':
+                            Console.Write("...");
+                            break;
+                        case 'S':
+                            Console.Write("///");
+                            break;
+                        case ' ':
+                            Console.Write("   ");
+                            break;
+                    }
 
                 Console.WriteLine();
             }
@@ -80,13 +95,28 @@ internal class Program
                     toCol = playerCol + 1;
                     direction = Direction.Right;
                     break;
+                case ConsoleKey.R:
+                    correctBoxCount = 0;
+                    currentLevel.Reset();
+                    return true;
+                case ConsoleKey.L:
+                    if (mapIndex == levels.Count - 1) return true;
+                    mapIndex++;
+                    currentLevel = levels[mapIndex];
+                    return true;
+                case ConsoleKey.K:
+                    if (mapIndex == 0) return true;
+                    mapIndex--;
+                    currentLevel = levels[mapIndex];
+                    return true;
                 case ConsoleKey.Escape:
+                case ConsoleKey.Q:
                     return false;
             }
 
             if (IllegalMove(toRow, toCol)) return true;
 
-            if (map[toRow, toCol] == 'B')
+            if (currentLevel.GetTile(toRow, toCol) == 'B')
             {
                 var boxToRow = playerRow;
                 var boxToCol = playerCol;
@@ -107,30 +137,47 @@ internal class Program
                 }
 
                 if (IllegalBoxMove(boxToRow, boxToCol)) return true;
-                map[boxToRow, boxToCol] = 'B';
-                map[toRow, toCol] = '0';
+                if (map[toRow, toCol] != 'S' && map[boxToRow, boxToCol] == 'S')
+                {
+                    correctBoxCount++;
+                    if (correctBoxCount == levels[mapIndex].BoxCount)
+                    {
+                        correctBoxCount = 0;
+                        mapIndex++;
+                        if (mapIndex == levels.Count)
+                        {
+                            Console.Clear();
+                            Console.WriteLine(" You Win!");
+                            return false;
+                        }
+
+                        currentLevel = levels[mapIndex];
+                        return true;
+                    }
+                }
+
+                if (map[toRow, toCol] == 'S' && map[boxToRow, boxToCol] != 'S') correctBoxCount--;
+
+                currentLevel.BoxPositions[currentLevel.FindBox(toRow, toCol)] = new Position(boxToRow, boxToCol);
             }
 
-            playerRow = toRow;
-            playerCol = toCol;
-
+            currentLevel.PlayerPosition = new Position(toRow, toCol);
             return true;
-        }
 
-        bool IllegalBoxMove(int toRow, int toCol)
-        {
-            if (toRow < 0 || toRow >= map.GetLength(0) || toCol < 0 || toCol >= map.GetLength(1) ||
-                map[toRow, toCol] != '0') return true;
+            bool IllegalBoxMove(int row, int col)
+            {
+                if (row < 0 || row >= map.GetLength(0) || col < 0 || col >= map.GetLength(1) ||
+                    currentLevel.GetTile(row, col) == '#' || currentLevel.GetTile(row, col) == 'B') return true;
+                return false;
+            }
 
-            return false;
-        }
+            bool IllegalMove(int row, int col)
+            {
+                if (row < 0 || row >= map.GetLength(0) || col < 0 || col >= map.GetLength(1) ||
+                    currentLevel.GetTile(row, col) == '#') return true;
 
-        bool IllegalMove(int toRow, int toCol)
-        {
-            if (toRow < 0 || toRow >= map.GetLength(0) || toCol < 0 || toCol >= map.GetLength(1) ||
-                map[toRow, toCol] == '#') return true;
-
-            return false;
+                return false;
+            }
         }
     }
 
@@ -138,11 +185,75 @@ internal class Program
     {
         public Level(char[,] map)
         {
-            Map = map;
+            OriginalMap = map.Clone() as char[,];
+            Map = map.Clone() as char[,];
+            PlayerPosition = new Position(0, 0);
+            BoxPositions = new List<Position>();
+            for (var row = 0; row < map.GetLength(0); row++)
+            for (var col = 0; col < map.GetLength(1); col++)
+            {
+                if (map[row, col] == 'P')
+                {
+                    PlayerPosition = new Position(row, col);
+                    Map[row, col] = '0';
+                }
+
+                if (map[row, col] == 'B')
+                {
+                    BoxPositions.Add(new Position(row, col));
+                    Map[row, col] = '0';
+                }
+            }
         }
 
-        public char[,] Map { get; set; }
+        public char[,] Map { get; private set; }
+        private char[,] OriginalMap { get; }
+        public Position PlayerPosition { get; set; }
+        public List<Position> BoxPositions { get; set; }
+        public int BoxCount => BoxPositions.Count;
+
+        public char GetTile(int row, int col)
+        {
+            if (BoxPositions.Exists(box => box.Row == row && box.Col == col)) return 'B';
+            if (PlayerPosition.Col == col && PlayerPosition.Row == row) return 'P';
+            return Map[row, col];
+        }
+
+        public int FindBox(int row, int col)
+        {
+            var ind = 0;
+            while (ind < BoxCount)
+            {
+                if (BoxPositions[ind].Row == row && BoxPositions[ind].Col == col) return ind;
+                ind++;
+            }
+
+            return -1;
+        }
+
+        public void Reset()
+        {
+            Map = OriginalMap.Clone() as char[,];
+            BoxPositions = new List<Position>();
+            for (var row = 0; row < OriginalMap.GetLength(0); row++)
+            for (var col = 0; col < OriginalMap.GetLength(1); col++)
+            {
+                if (OriginalMap[row, col] == 'P')
+                {
+                    PlayerPosition = new Position(row, col);
+                    Map[row, col] = '0';
+                }
+
+                if (OriginalMap[row, col] == 'B')
+                {
+                    BoxPositions.Add(new Position(row, col));
+                    Map[row, col] = '0';
+                }
+            }
+        }
     }
+
+    private record Position(int Row, int Col);
 
     private enum Direction
     {
--- ConsoleGame2/obj/Debug/net9.0/ConsoleGame2.AssemblyInfo.cs
+++ ConsoleGame2/obj/Debug/net9.0/ConsoleGame2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleGame2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53deb500de8a1db7c0762defa4333f98b818f760")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleGame2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleGame2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
